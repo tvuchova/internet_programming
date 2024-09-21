bfbb3c059b3dfbdd65e543d43226d889296f7aa4
tvuchova <temenuzhka.vuchova@rewe-digital.com>
2024-09-21 17:04:57 +0300

    initial project

---
+++ lesson_1/commands/NSLookup.java
@@ -0,0 +1,7 @@
+package commands;
+
+public class NSLookup {
+    public static void main(String[] args) {
+
+    }
+}
--- lesson_1/sockets/Client.java
+++ lesson_1/sockets/Client.java
@@ -1,3 +1,5 @@
+package sockets;
+
 public class Client {
     public static void main(String[] args) {
         System.out.println("Hello World.I am Client!");
