ee1c36922d7a3c9b2c6e3f850b93069fb9344073
tvuchova <temenuzhka.vuchova@rewe-digital.com>
2024-09-27 17:31:11 +0300

    refactor

--- lesson_1/sockets_part3/finalized/simple_client_server/ClientFinal.java
+++ lesson_1/sockets_part3/finalized/simple_client_server/ClientFinal.java
@@ -10,17 +10,17 @@ public class ClientFinal {
     public static void main(String[] args) {
         System.out.println("Hello World.I am Client!");
         try {
-            Socket socket = new Socket("localhost",9708);
+            Socket socket = new Socket("localhost", 9708);
             //we need to read data in from keyboard and sent it to the
             try (BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
                 System.out.println("Enter string");
                 String str = userInput.readLine();
-                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 out.println(str);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 System.out.println(in.readLine());
             }
-            socket.close( );
+            socket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- lesson_1/sockets_part3/finalized/simple_client_server/ServerFinal.java
+++ lesson_1/sockets_part3/finalized/simple_client_server/ServerFinal.java
@@ -12,12 +12,12 @@ public class ServerFinal {
         System.out.println("Hello World.I am Server!");
 
         try {
-            ServerSocket ss =new ServerSocket(9708);
+            ServerSocket ss = new ServerSocket(9708);
             Socket soc = ss.accept();
             System.out.println("Connection established");
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
             String str = in.readLine();
-            PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
             out.println("Server echo says: " + str);
             ss.close();
 
