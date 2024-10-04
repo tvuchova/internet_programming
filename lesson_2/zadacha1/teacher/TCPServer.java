012b73c607c3cff695feb4dcaef944a15b53d9b4
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-10-04 20:33:28 +0300

    restructure project

--- lesson_2/zadacha1/teacher/TCPServer.java
+++ lesson_2/zadacha1/teacher/TCPServer.java
@@ -6,17 +6,18 @@ import java.net.Socket;
 
 public class TCPServer {
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(9238);
+        try (ServerSocket serverSocket = new ServerSocket(9238)) {
             System.out.println("Server is started on port 9238...");
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-
-                new Thread(new ClientHandler(clientSocket)).start();
+                try (Socket clientSocket = serverSocket.accept()) {
+                    // Handle the client connection in a separate thread
+                    new Thread(new ClientHandler(clientSocket)).start();
+                } catch (IOException e) {
+                    System.err.println("Error handling client connection: " + e.getMessage());
+                }
             }
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Error starting the server: " + e.getMessage());
         }
     }
 }
