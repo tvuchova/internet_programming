16c55067e354b57c7079b9de644b51aff27d8813
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-10-04 20:30:16 +0300

    restructure project

--- lesson_2/zadacha1/teacher/ClientHandler.java
+++ lesson_2/zadacha1/teacher/ClientHandler.java
@@ -9,6 +9,7 @@ import java.util.Enumeration;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
+
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
