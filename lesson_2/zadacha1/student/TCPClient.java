c07e25f5e9d4c290e4a8b8abfedaa9bf4765a7ba
yoskohaha <hourbin86@gmail.com>
2024-10-11 11:25:09 +0300

    Student side zadacha 1 hot-fix

--- lesson_2/zadacha1/student/TCPClient.java
+++ lesson_2/zadacha1/student/TCPClient.java
@@ -7,20 +7,20 @@ import java.util.Scanner;
 public class TCPClient {
     public static void main(String[] args) {
         try {
-            // Свързваме се със сървъра на локалната машина (localhost) и порт 1234
+
             Socket socket = new Socket("localhost", 1234);
             System.out.println("Свързване към сървъра...");
 
-            // Изходен поток за изпращане на данни към сървъра
+
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Входен поток за четене на отговори от сървъра
+
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // Вход от потребителя (конзолата)
+
             Scanner scanner = new Scanner(System.in);
 
-            // Четене на командата от потребителя
+
             System.out.println("Изберете команда (ADD, LIST, AVERAGE): ");
             String command = scanner.nextLine();
 
@@ -30,13 +30,13 @@ public class TCPClient {
                 System.out.println("Въведете оценката на студента: ");
                 int grade = Integer.parseInt(scanner.nextLine());
 
-                // Изпращаме команда за добавяне на студент
+
                 out.println("ADD," + name + "," + grade);
             } else if (command.equalsIgnoreCase("LIST")) {
-                // Изпращаме команда за преглед на списъка с оценки
+
                 out.println("LIST");
             } else if (command.equalsIgnoreCase("AVERAGE")) {
-                // Изпращаме команда за изчисляване на средна оценка
+
                 out.println("AVERAGE");
             } else {
                 System.out.println("Невалидна команда.");
@@ -44,13 +44,13 @@ public class TCPClient {
                 return;
             }
 
-            // Четене на отговорите от сървъра и показване на резултата на клиента
+
             String response;
             while ((response = in.readLine()) != null) {
                 System.out.println("Отговор от сървъра: " + response);
             }
 
-            // Затваряме ресурсите
+           
             in.close();
             out.close();
             socket.close();
