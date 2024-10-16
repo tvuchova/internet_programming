aebcacc6139f7145c542a358281e02b76ae09df8
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-10-16 13:50:27 +0300

    solve tasks from excercize

--- lesson_2/zadacha2_grades/teacher/Student.java
+++
@@ -1,9 +0,0 @@
-package lesson_2.zadacha2_grades.teacher;
-
-public record Student(String name, double grade) {
-
-    @Override
-    public String toString() {
-        return "Student" + name + " - Mark: " + grade;
-    }
-}
