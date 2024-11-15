3bc2094a7ba55e0e908e6990c13a4c292bb6c2ff
ValeriTodorov123 <valerytodorov5@gmail.com>
2024-11-15 14:19:18 +0200

    Update HttpServerTask.java

--- httpExamples/src/main/java/org/lecture/initial/HttpServerTask.java
+++ httpExamples/src/main/java/org/lecture/initial/HttpServerTask.java
@@ -5,18 +5,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class HttpServerTask {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    //private static User userState = new User();
+    private static User userState = new User();
 
     public static void main(String[] args) throws Exception {
+        Server server = new Server(8000);
+        server.setHandler(new RequestHandler());
 
+        server.start();
+        server.join();
     }
 
     static class RequestHandler extends AbstractHandler {
@@ -25,10 +31,23 @@ public class HttpServerTask {
 
             switch (target) {
                 case "/hello":
-
+                    if("GET".equalsIgnoreCase(request.getMethod())) {
+                        handleHello(response);
+                    } else {
+                        handleMethodNotAllowed(response);
+                    }
                     break;
 
                 case "/user":
+                    if("POST".equalsIgnoreCase(request.getMethod())) {
+                        handleCreateUser(request, response);
+                    } else if("PUT".equalsIgnoreCase(request.getMethod())) {
+                        handleReplaceUser(request, response);
+                    } else if("PATCH".equalsIgnoreCase(request.getMethod())) {
+                        handlePatchUser(request, response);
+                    } else {
+                        handleMethodNotAllowed(response);
+                    }
 
                     break;
 
@@ -45,18 +64,62 @@ public class HttpServerTask {
         }
 
         private void handleHello(HttpServletResponse response) throws IOException {
-
+            String message = "Hello World, geicho";
+            response.setContentType("text/plain");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(message);
         }
 
         private void handleCreateUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            try {
+                User user = objectMapper.readValue(request.getInputStream(), User.class);
+                synchronized (HttpServerTask.class) {
+                    UUID uuid = UUID.randomUUID();
+                    user.setId(uuid);
+                    userState = user;
+                }
+                response.setContentType("application/json;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.getWriter().println(objectMapper.writeValueAsString(userState));
+            }catch(Exception e){
+                    handleBadRequest(response);
+            }
 
         }
 
         private void handleReplaceUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            try {
+                synchronized (HttpServerTask.class) {
+                    userState.setStatus("true");
+                    userState.setName("");
+                    userState.setAge(0);
+                    userState.setEmail("");
+                    userState.setId(null);
+                }
+                response.setContentType("application/json;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println(objectMapper.writeValueAsString(userState));
+            }catch(Exception e){
+                handleBadRequest(response);
+            }
         }
 
         private void handlePatchUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+            try {
+                User user = objectMapper.readValue(request.getInputStream(), User.class);
+                synchronized (HttpServerTask.class) {
+                    if(user.getName() != null) userState.setName(user.getName());
+                    if(user.getAge() != 0) userState.setAge(user.getAge());
+                    if(user.getEmail() != null) userState.setEmail(user.getEmail());
+                    if(user.getStatus() != null) userState.setStatus(user.getStatus());
+                    if(user.getId() != null) userState.setId(user.getId());
+                }
+                response.setContentType("application/json;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println(objectMapper.writeValueAsString(userState));
+            }catch(Exception e){
+                handleBadRequest(response);
+            }
         }
 
         private void handleMethodNotAllowed(HttpServletResponse response) throws IOException {
